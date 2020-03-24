ba2e43927c2f5a916114a3ffdd442a8fd2e7740b
Antonio Dell'Elce <antouk@dellelce.com>
2020-03-24 02:28:04 +0000

    restyle comments; add "rawdata" and "simple" variables

--- clinvar.py
+++ clinvar.py
@@ -1,37 +1,56 @@
 """
- Compare a DNA kit (23andMe, AncestryDNA, etc) with the Clinvar database
+ Compare a DNA kit (23andMe, AncestryDNA, etc) with the Clinvar database.
 
-vcf line:
+example vcf line:
 
-1       865628  789256  G       A       .       .       ALLELEID=707587;CLNDISDB=MedGen:CN517202
-;CLNDN=not_provided;CLNHGVS=NC_000001.10:g.865628G>A;CLNREVSTAT=criteria_provided,_single_submit
-ter;CLNSIG=Likely_benign;CLNVC=single_nucleotide_variant;CLNVCSO=SO:0001483;GENEINFO=SAMD11:1483
-98;MC=SO:0001583|missense_variant;ORIGIN=1
+1       865628  789256  G       A       .       .
+
+  ALLELEID=707587
+  CLNDISDB=MedGen:CN517202
+  CLNDN=not_provided
+  CLNHGVS=NC_000001.10:g.865628G>A
+  CLNREVSTAT=criteria_provided,_single_submitter
+  CLNSIG=Likely_benign
+  CLNVC=single_nucleotide_variant
+  CLNVCSO=SO:0001483
+  GENEINFO=SAMD11:148398;
+  MC=SO:0001583|missense_variant
+  ORIGIN=1
 """
 
 
 class dnadata(object):
+    """Reader to personal DNA Data."""
+
     dna_providers = ['myheritage', 'livingdna', 'ancestry', '23andme']
 
     def __init__(self, name, provider=None):
-        """read specified dna file"""
+        """Read specified dna file."""
         pass
 
 
 class vcf(object):
+    """Wrapper class to the Clinvar database in VCF format."""
+
     base_url = 'ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37'
 
     def process_file(self, name):
-        """process file in vcf format"""
-
+        """Process file in vcf format."""
         with open(name) as fh:
             vcf = self.process(fh.read())
 
         return vcf
 
-    def process(self, stream):
-        """process stream in vcf format"""
+    def process(self, stream=None):
+        """Process stream in vcf format."""
         vcf = []
+
+        if stream is None:
+            if self.rawdata is None:
+                self.rawdata = self.latest()
+                stream = self.rawdata
+            else:
+                stream = self.rawdata
 
         vcf_raw = stream.splitlines()
 
@@ -47,8 +66,8 @@ class vcf(object):
 
         return vcf
 
-    def simple_dict(self, vcf):
-        """return a dictionary using chromosome and position in chromosome as keys"""
+    def simple_dict(self, vcf=None):
+        """Return a dictionary using chromosome and position in chromosome as keys."""
         vcf_dict = {}
 
         for line in vcf:
@@ -71,10 +90,12 @@ class vcf(object):
         return vcf_dict
 
     def __init__(self, name=None):
-        pass
+        """Set-up object."""
+        self.rawdata = None
+        self.simple = None
 
     def latest(self):
-        """download latest available copy of clinvar database in vcf format"""
+        """Download latest available copy of clinvar database in vcf format."""
         import requests
         import requests_ftp
         import gzip
@@ -83,10 +104,12 @@ class vcf(object):
         with requests.Session() as sess:
             resp = sess.get('{}/clinvar.vcf.gz'.format(self.base_url))
 
-        return gzip.decompress(resp.content)
+        self.rawdata = gzip.decompress(resp.content)
+
+        return self.rawdata
 
     def download(self, name=None, workdir=None):
-        """download current or specified version of clinvar database"""
+        """Download current or specified version of clinvar database."""
         pass
 
 
